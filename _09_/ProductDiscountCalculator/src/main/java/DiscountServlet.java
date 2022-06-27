dc848d9416cceaa1eaf3ba44b4d3e1d1940cceae
Thành Tri <apple@ThanhTri.local>
2022-06-27 20:41:44 +0700

    27/06

---
+++ _09_/ProductDiscountCalculator/src/main/java/DiscountServlet.java
@@ -0,0 +1,28 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DiscountServlet", value = "/display_discount")
+public class DiscountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productDescription = request.getParameter("productDescription");
+        double listPrice = Double.parseDouble(request.getParameter("listPrice"));
+        double discountPercent = Double.parseDouble(request.getParameter("discountPercent"));
+
+        double discountAmount = listPrice *discountPercent*0.01;
+        double payPrice = listPrice -discountAmount;
+
+        request.setAttribute("productDescription", productDescription);
+        request.setAttribute("discountAmount", discountAmount);
+        request.setAttribute("payPrice", payPrice);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
---
+++ _09_/ProductDiscountCalculator/src/main/webapp/index.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: apple
+  Date: 27/06/2022
+  Time: 16:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h2>Product</h2>
+  <form method="post" action="/display_discount">
+    <label>Product Description: </label><br/>
+    <label><input type="text" name="productDescription" placeholder="Product Description"></label><br/>
+    <label>List Price: </label><br/>
+    <label><input type="text" name="listPrice" placeholder="0"></label><br/>
+    <label>Discount Percent: </label><br/>
+    <label><input type="text" name="discountPercent" placeholder="0"></label><br/>
+    <input type="submit" value="Calculate Discount">
+  </form>
+  <h3> Product Description: ${productDescription}</h3>
+  <h3> Discount Amount: ${discountAmount}</h3>
+  <h3> Pay Price: ${payPrice}</h3>
+  </body>
+</html>
